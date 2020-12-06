67bc05a0715c25ba650a52389777bbde15f5a5c5
Azahar Hossain <azahar.hossain10@yahoo.com>
2020-12-06 16:56:32 +0100

    TD: On submit shows form values in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+  defaultSubscription = 'advanced';
+  formSubmitted = false;
+  formData = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+   @ViewChild('f', {static: false})signUpForm: NgForm;
+  onSubmit(f: NgForm) {
+    this.formSubmitted = true;
+   // console.log('User input' + this.signUpForm.value);
+    console.log(f.value);
+    this.formData.email = this.signUpForm.value.email;
+    this.formData.subscription = this.signUpForm.value.subscription;
+    this.formData.password = this.signUpForm.value.password;
 
+  }
 }
